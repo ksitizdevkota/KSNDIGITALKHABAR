480fd34bbd1a3ae0bc7598cf157f5fad543bfcaf
NimeshPdl <nimeshpaudel1025@gmail.com>
2020-06-18 00:31:01 +0545

    starting login form for admin

---
+++ ksndigital/src/component/AdminLoginForm.js
@@ -0,0 +1,17 @@
+// @flow strict
+
+import * as React from 'react';
+
+type Props = {||};
+
+class AdminLoginForm extends React.Component<Props> {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AdminLoginForm;
